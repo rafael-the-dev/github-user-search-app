a743679be2a2cf757836ba50bcf81c96a2fea958
Rafael Tivane <luislangabusiness@gmail.com>
2022-01-11 20:16:47 +0200

    adding global styles to components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,13 @@
-import { Avatar, Button, IconButton, Paper, Typography } from '@mui/material'
+import { Avatar, Button, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Paper, Typography } from '@mui/material'
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import SearchIcon from '@mui/icons-material/Search';
 import { useDisplay, useTypography } from '../../styles';
 import classNames from 'classnames';
 import { useStyles } from './styles';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import LinkIcon from '@mui/icons-material/Link';
 
 const Home = () => {
     const display = useDisplay();
@@ -20,7 +24,7 @@ const Home = () => {
                     variant="h5">
                     devfinder
                 </Typography>
-                <Button endIcon={<DarkModeIcon />}>
+                <Button endIcon={<DarkModeIcon />} className={classNames(classes.lightText, text.font7)}>
                     Dark
                 </Button>
             </header>
@@ -28,10 +32,10 @@ const Home = () => {
                 <Paper component="form" className={classNames(display.alignCenter, display.flex, classes.px, 
                     classes.defaultBorderRadius, classes.form)}>
                     <IconButton>
-                        <SearchIcon />
+                        <SearchIcon classes={{ root: classNames(classes.blueText)}} />
                     </IconButton>
                     <input 
-                        className={classNames(display.borderNone, display.flexGrow1, display.outlineNone)}
+                        className={classNames(display.borderNone, display.flexGrow1, display.outlineNone, classes.lightText)}
                         placeholder="Search GitHub username…"
                     />
                     <Button 
@@ -48,28 +52,63 @@ const Home = () => {
                             alt="profile"
                         />
                         <div className={classNames(display.flex, display.flexColumn, display.ml1)}>
-                            <Typography component="h2" variant="h6" className={classNames(text.font7)}>The Octocat</Typography>
-                            <Typography className={classNames(text.rem9)}>octocat</Typography>
-                            <Typography variant="body2">Joined 25 Jan 2011</Typography>
+                            <Typography 
+                                component="h2" 
+                                variant="h6" 
+                                className={classNames(text.font7, classes.dark1Text)}>
+                                The Octocat
+                            </Typography>
+                            <Typography 
+                                gutterBottom 
+                                className={classNames(text.rem9, classes.blueText)}>
+                                @octocat
+                            </Typography>
+                            <Typography variant="body2" className={classNames(classes.lightText)}>Joined 25 Jan 2011</Typography>
                         </div>
                     </div>
-                    <Typography variant="body2" className={classNames(display.mt2)}>
+                    <Typography variant="body2" className={classNames(display.mt2, text.rem9, classes.lightText)}>
                         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.
                     </Typography>
-                    <div className={classNames(display.flex, display.justifyBetween, display.mt1, classes.panel)}>
+                    <div className={classNames(display.flex, display.justifyBetween, display.mt2, classes.panel)}>
                         <Typography variant="body2" className={classNames(display.flex, display.flexColumn, display.alignCenter)}>
-                            <span>Repos</span>
-                            <span className={classNames(text.font7, display.mt1, classes.panelValue)}>8</span>
+                            <span className={classNames(classes.lightText)}>Repos</span>
+                            <span className={classNames(text.font7, display.mt1, classes.panelValue, classes.dark1Text)}>8</span>
                         </Typography>
                         <Typography variant="body2" className={classNames(display.flex, display.flexColumn, display.alignCenter)}>
-                            <span>Followers</span>
-                            <span className={classNames(text.font7, display.mt1, classes.panelValue)}>3938</span>
+                            <span className={classNames(classes.lightText)}>Followers</span>
+                            <span className={classNames(text.font7, display.mt1, classes.panelValue, classes.dark1Text)}>3938</span>
                         </Typography>
                         <Typography variant="body2" className={classNames(display.flex, display.flexColumn, display.alignCenter)}>
-                            <span>Following</span>
-                            <span className={classNames(text.font7, display.mt1, classes.panelValue)}>9</span>
+                            <span className={classNames(classes.lightText)}>Following</span>
+                            <span className={classNames(text.font7, display.mt1, classes.panelValue, classes.dark1Text)}>9</span>
                         </Typography>
                     </div>
+                    <List  disablePadding className={classNames(display.mt2)}>
+                        <ListItemButton component="li" className={classNames(display.pl0, classes.listItemButton)}>
+                            <ListItemIcon classes={{ root: classNames(classes.listIcon, classes.lightText)}}>
+                                <LocationOnIcon />
+                            </ListItemIcon>
+                            <ListItemText classes={{ primary: text.rem9}} className={classNames(classes.lightText)} primary="San Francisco" />
+                        </ListItemButton>
+                        <ListItemButton component="li" className={classNames(display.pl0, classes.listItemButton)}>
+                            <ListItemIcon classes={{ root: classNames(classes.listIcon, classes.lightText)}}>
+                                <LinkIcon />
+                            </ListItemIcon>
+                            <ListItemText classes={{ primary: text.rem9}} className={classNames(classes.lightText)} primary="https://github.blog" />
+                        </ListItemButton>
+                        <ListItemButton component="li" className={classNames(display.pl0, classes.listItemButton)}>
+                            <ListItemIcon classes={{ root: classNames(classes.listIcon, classes.lightText)}}>
+                                <TwitterIcon />
+                            </ListItemIcon>
+                            <ListItemText classes={{ primary: text.rem9}} className={classNames(classes.lightText)} primary="Not Available" />
+                        </ListItemButton>
+                        <ListItemButton component="li" className={classNames(display.pl0, classes.listItemButton)}>
+                            <ListItemIcon classes={{ root: classNames(classes.listIcon, classes.lightText)}}>
+                                <ApartmentIcon />
+                            </ListItemIcon>
+                            <ListItemText classes={{ primary: text.rem9}} className={classNames(classes.lightText)} primary="@github" />
+                        </ListItemButton>
+                    </List>
                 </Paper>
             </main>
         </div>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,15 +1,15 @@
 import { makeStyles } from "@mui/styles";
 import { colors } from '../../styles/colors';
 
-const  { lightGrey } = colors;
+const  { blue, dark1, darkLightGrey, lightGrey } = colors;
 
 export const useStyles = makeStyles(theme => ({
     main: {
         backgroundColor: '#F2F2F2'
     },
     px: {
-        paddingLeft: '4% !important',
-        paddingRight: '4% !important'
+        paddingLeft: '5% !important',
+        paddingRight: '5% !important'
     },
     defaultBorderRadius: {
         borderRadius: '14px !important',
@@ -25,12 +25,32 @@ export const useStyles = makeStyles(theme => ({
         height: '60px !important',
         width: '60px !important'
     },
+    lightText: {
+        color: `${darkLightGrey} !important`
+    },
+    blueText: {
+        color: `${blue} !important`
+    },
+    dark1Text: {
+        color: `${dark1} !important`
+    },
+    description: {
+        
+    },
     panel: {
         background: lightGrey,
         borderRadius: 10,
         padding: '1rem 31px',
     },
     panelValue: {
-        fontSize: '1rem'
+        fontSize: '1.02rem'
+    },
+    listItemButton: {
+        '&:hover': {
+            backgroundColor: 'transparent !important'
+        }
+    },
+    listIcon: {
+        minWidth: '43px !important'
     }
 }));
